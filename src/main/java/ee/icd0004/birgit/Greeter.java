d971b7ee71fe936f569c3eec37edc662833b05c3
birgit1000 <birgitkruus@hotmail.com>
2020-10-06 14:13:30 -0700

    REQ 2 : refactored

--- src/main/java/ee/icd0004/birgit/Greeter.java
+++ src/main/java/ee/icd0004/birgit/Greeter.java
@@ -3,7 +3,14 @@ package ee.icd0004.birgit;
 public class Greeter {
     
     public String greet(String name){
-        if (name == null) return "Hello, my friend.";
+        String defaultName = "my friend";
+        if (isEmpty(name)) {
+            return String.format("Hello, %s.", defaultName);
+        }
         return String.format("Hello, %s.", name);
+    }
+
+    public boolean isEmpty(String name){
+        return name == null;
     }
 }
