de2afc7a2e9766f33665bbc4142d19947cbb24cc
birgit1000 <birgitkruus@hotmail.com>
2020-10-06 13:58:26 -0700

    REQ 1 : added implementation

---
+++ src/test/java/ee/icd0004/birgit/GreeterTest.java
@@ -0,0 +1,35 @@
+package ee.icd0004.birgit;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class GreeterTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( false );
+    }
+
+    @Test
+    public void shouldReturnGreetingWithGivenName(){
+        //arrange
+        Greeter greeter = new Greeter();
+        String name = "Bob";
+        String expectedGreetingMessage = "Hello, Bob";
+
+        //act
+        String actualGreetingMessage = greeter.greet(name);
+
+        //assert
+        assertEquals(expectedGreetingMessage, actualGreetingMessage);
+    }
+}
