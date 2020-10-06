80765465e48bbd3c8e7fd12f3d4bf793c3bfbb99
birgit1000 <birgitkruus@hotmail.com>
2020-10-06 15:57:05 -0700

    REQ 6 : added implementation

--- src/main/java/ee/icd0004/birgit/Greeter.java
+++ src/main/java/ee/icd0004/birgit/Greeter.java
@@ -1,5 +1,8 @@
 package ee.icd0004.birgit;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Greeter {
     
     public String greet(String name){
@@ -16,31 +19,62 @@ public class Greeter {
     public String greet(String[] names){
         StringBuilder sb = new StringBuilder();
         sb.append("Hello,");
-        int lastNameIndex = names.length - 1;
+        Queue<String> upperCaseNames = new LinkedList<>();
+        Queue<String> lowerCaseNames = new LinkedList<>();
+        divideUpperAndLowerCaseNames(upperCaseNames, lowerCaseNames, names);
 
-        appendNamesToGreeting(names, sb, lastNameIndex);
+        appendLowerCaseNamesToGreeting(lowerCaseNames, sb);
+        if(!upperCaseNames.isEmpty()) appendUpperCaseNamesToGreeting(upperCaseNames, sb);
         
         return sb.toString();
     }
 
-    private void appendNamesToGreeting(String[] names, StringBuilder sb, int lastNameIndex) {
-        for(int i = 0; i < names.length; i++){
-            appendEnding(names[i], sb, lastNameIndex, i);
+    private void appendUpperCaseNamesToGreeting(Queue<String> upperCaseNames, StringBuilder sb) {
+        StringBuilder upperCaseSb = new StringBuilder();
+        int lastNameIndex = upperCaseNames.size() - 1;
+        boolean isOxfordCommaNeeded = checkIfOxfordCommaIsNeeded(upperCaseNames);
+        sb.append(" AND HELLO,");
+
+        while(!upperCaseNames.isEmpty()){
+            appendEnding(upperCaseNames.peek() , upperCaseSb, lastNameIndex, isOxfordCommaNeeded);
+            upperCaseNames.remove();
+        }
+        
+        upperCaseSb.append("!");
+        String upperCaseGreeting = upperCaseSb.toString().toUpperCase();
+        sb.append(upperCaseGreeting);
+    }
+
+    private boolean checkIfOxfordCommaIsNeeded(Queue<String> upperCaseNames) {
+        return upperCaseNames.size() > 2;
+    }
+
+    private void divideUpperAndLowerCaseNames(Queue<String> upperCaseNames, Queue<String> lowerCaseNames,
+            String[] names) {
+         for(String name : names){
+            if(isUpperCase(name)) upperCaseNames.add(name);
+            else lowerCaseNames.add(name);
         }
     }
 
-    private void appendEnding(String name, StringBuilder sb, int lastNameIndex, int i) {
-        if(i != lastNameIndex) {
+    private void appendLowerCaseNamesToGreeting(Queue<String> lowerCaseNames, StringBuilder sb) {
+        boolean isOxfordCommaNeeded = checkIfOxfordCommaIsNeeded(lowerCaseNames);
+
+        while(!lowerCaseNames.isEmpty()){
+            appendEnding(lowerCaseNames.peek() , sb, lowerCaseNames.size(), isOxfordCommaNeeded);
+            lowerCaseNames.remove();
+        }
+
+    }
+
+    private void appendEnding(String name, StringBuilder sb, int listSize, boolean isOxfordCommaNeeded) {
+        if(listSize != 1) {
             sb.append(" ").append(name);
-            if(isMoreThan2Names(lastNameIndex)) sb.append(",");
+            if(listSize > 1 && isOxfordCommaNeeded) sb.append(",");
         }
         else {
             sb.append(" and ").append(name).append(".");;
         }
-    }
-
-    private boolean isMoreThan2Names(int lastNameIndex) {
-        return lastNameIndex != 1;
     }
 
     private String getGreeting(String name) {
