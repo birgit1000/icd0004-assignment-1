e69db9180be2dc600643e86e391e7c68dc63b79e
birgit1000 <birgitkruus@hotmail.com>
2020-10-06 14:08:37 -0700

    REQ 2 : added implementation

--- src/main/java/ee/icd0004/birgit/Greeter.java
+++ src/main/java/ee/icd0004/birgit/Greeter.java
@@ -3,6 +3,7 @@ package ee.icd0004.birgit;
 public class Greeter {
     
     public String greet(String name){
+        if (name == null) return "Hello, my friend.";
         return String.format("Hello, %s.", name);
     }
 }
